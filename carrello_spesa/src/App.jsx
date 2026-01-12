fe727291b32bab6476474920da2905a2f55417ac
SergioGBosco <sergiogibosco@gmail.com>
2026-01-12 17:49:05 +0100

    Milestone 2 - UseState fro show added products

--- carrello_spesa/src/App.jsx
+++ carrello_spesa/src/App.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
 
 
 function App() {
+
+  const [addedProducts, setAddedProducts] = useState([]);
+  console.log(addedProducts)
+
+  const addToCart = product => {
+    const prodottoAggiungo = addedProducts.some(p => p.name === product.name);
+    if (prodottoAggiungo) {
+      return
+    }
+    setAddedProducts(valoreAttuale => [...valoreAttuale, {
+      ...product,
+      quantity: 1
+    }])
+  }
+
   const products = [
     { name: 'Mela', price: 0.5 },
     { name: 'Pane', price: 1.2 },
@@ -15,9 +31,22 @@ function App() {
         {products.map((p, i) => (
           <li key={i}>
             {p.name} ({p.price.toFixed(2)}€)
+            <button onClick={() => addToCart(p)}>Aggiungi al Carrello</button>
           </li>
         ))}
       </ul>
+      {addedProducts.length > 0 && (<>
+        <h2>Carrello della spesa</h2>
+        <ul>
+          {addedProducts.map((p, i) => (
+            <li key={i}>
+              <p>{p.quantity} x {p.name} ({p.price.toFixed(2)}€)</p>
+            </li>
+          ))}
+        </ul>
+      </>)}
+
+
     </>
   )
 }
